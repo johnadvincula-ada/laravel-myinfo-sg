2dd4299e25a1d3e85376ec02c5ff53cbd90e4aeb
johnadvincula-ada <89056034+johnadvincula-ada@users.noreply.github.com>
2022-01-31 22:15:13 +0800

    Update MyinfoSecurityService.php

--- src/Services/MyinfoSecurityService.php
+++ src/Services/MyinfoSecurityService.php
@@ -31,7 +31,7 @@ final class MyinfoSecurityService
     public static function verifyJWS(string $accessToken)
     {
         $algorithmManager = new AlgorithmManager([new RS256]);
-        $jwk = JWKFactory::createFromCertificateFile(config('laravel-myinfo-sg.public_cert_path'));
+        $jwk = JWKFactory::createFromCertificateFile('file://' . storage_path(config('laravel-myinfo-sg.public_cert_path')));
         $jwsVerifier = new JWSVerifier($algorithmManager);
         $serializerManager = new JWSSerializerManager([new CompactSerializer]);
 
@@ -106,7 +106,7 @@ final class MyinfoSecurityService
             Log::debug('Base String (Pre Signing): '.$baseString);
         }
 
-        $privateKey = openssl_pkey_get_private(config('laravel-myinfo-sg.private_key_path'), $passphrase);
+        $privateKey = openssl_pkey_get_private('file://' . storage_path(config('laravel-myinfo-sg.private_key_path')), $passphrase);
 
         openssl_sign($baseString, $signature, $privateKey, 'sha256WithRSAEncryption');
 
